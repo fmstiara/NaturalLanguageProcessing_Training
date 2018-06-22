c836fa0fbe27699cd4f0a9363792a4a14aab133b
TairaOuchi <taira.o19@gmail.com>
2018-06-22 15:57:44 +0900

    02までクリア

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 http://www.cl.ecei.tohoku.ac.jp/nlp100/#ch1
 
 Python初心者が上記サイトを参考にトレーニング
+
+# 参考文献
+- [Python文字列操作マスター][https://qiita.com/tomotaka_ito/items/594ee1396cf982ba9887]
+- [[Python]for文処理が1行で書ける!素敵なリスト内包表記][https://www.yoheim.net/blog.php?q=20150702]
--- warm_up.py
+++ warm_up.py
@@ -1,1 +1,26 @@
 # 準備運動編
+
+# 文字列反転
+# str[start:end:step]
+
+s1 = "template text"
+print(s1[::-1])
+
+
+# 奇数文字目を取り出し
+s2 = "パタトクカシーーー"
+print(s2[::2])
+
+# 偶数文字ならスタートをずらす
+print(s2[1::2])
+
+
+# 文字列を交互に連結
+p = "パトカー"
+t = "タクシー"
+
+# 'x'.join([])で[]をxで連結する
+print(''.join([char1 + char2 for char1, char2 in zip(p, t)]))
+
+# 円周率
+s3 = "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."
